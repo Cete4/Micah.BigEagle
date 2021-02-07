eaa1c13c95e30e2b254001ac8b9a357f3f7631a6
Cete4 <46322113+Cete4@users.noreply.github.com>
2021-02-07 16:37:16 -0600

    Changed app to have pages and path accordingly

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home' // import our pages 
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Home' component={Home} />
+          <Route exact path='/Contact' component={Contact} />
+          <Route exact path='/Portfolio' component={Home} />
+          <Route exact path='/Work' component={Portfolio} />
+        </Switch>
+      </Router>
     </div>
   );
 }
